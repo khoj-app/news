06a2568dbddb193ffb1fa6453dbee3fc09fefcde
Honey Malviya <hi@honeymalviya.com>
2023-05-23 12:26:28 +0200

    update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   url: 'https://khoj-app.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/news/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName, // Usually your GitHub org/user name.
